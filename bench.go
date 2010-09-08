0f1b9ba24e09b358c04286ae00deed1fe096d86d
David Roundy <roundyd@physics.oregonstate.edu>
2010-09-08 10:04:37 -0400

    add benchmark timing on morland (which is *very* slow!)
    Tested-on: morland (Linux 2.6.32-5-686)

--- bench.go
+++ bench.go
@@ -21,7 +21,7 @@ func main() {
 	sec2, nsec2, _ := os.Time()
 	name, _ := os.Hostname()
 	dt := float64(sec2-sec) + 1e-9*float64(nsec2-nsec)
-	timelimits := map[string]float64{"collins": 5}
+	timelimits := map[string]float64{"collins": 5, "morland": 60}
 	fmt.Println("It took ", dt,"seconds on", name)
 	expected, ok := timelimits[name]
 	if ok {
